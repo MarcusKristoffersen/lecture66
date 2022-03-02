cb5d2238cad5d0f470cc426f822c47a366aa38a0
MarcusKri <marcus-kristoffersen@outlook.com>
2022-03-02 14:57:42 +0100

    Serve React from Express

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.get("/api/login", (req, res) => {
   res.json({ username: "admin", fullName: "Noen Àndre Persson" });
 });
+
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });
