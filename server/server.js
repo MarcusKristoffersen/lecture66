495a45e7ab8d07b8c10f038d3c7c6e12dc3f9172
MarcusKri <marcus-kristoffersen@outlook.com>
2022-03-02 13:55:45 +0100

    Dummy get endpoint in express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/login", (req, res) => {
+  res.json({ username: "admin", fullName: "Noen Àndre Persson" });
+});
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });
